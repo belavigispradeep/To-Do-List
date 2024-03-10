3451f2f61a7f33b791c219875e484eb8e9e3c6a2
belavigispradeep <pradeepsbelavigi26@gmail.com>
2024-03-10 19:08:06 +0530

    removed the comments

--- todo.js
+++ todo.js
@@ -5,29 +5,22 @@ function addTask(){
     if(taskText !==""){
         const textList = document.querySelector("#taskList");
 
-        //create new task element;
         const taskElement = document.createElement("div");
         taskElement.classList.add("task");
 
-        //create task text;
         const taskTextElement = document.createElement("span");
         taskTextElement.innerText = taskText;
 
-        //create Delete button;
         const deleteBtn = document.createElement("button");
         deleteBtn.innerText = "Delete";
         deleteBtn.onclick = function (){
             taskList.removeChild(taskElement);
         }
 
-        //Append elements to the task container;
         taskElement.appendChild(taskTextElement);
         taskElement.appendChild(deleteBtn);
-
-        //Append the task container to the task list;
         taskList.appendChild(taskElement);
 
-        //Clear input field;
         taskinput.value = "";
     }
 }
